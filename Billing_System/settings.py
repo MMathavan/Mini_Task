4f0042cb9fc5808dc6b43bf09ac419465c84f599
fusiontecsupport <support@fusiontec.com>
2026-03-05 10:10:04 +0530

    change db from postgresql to sqlite3

--- Billing_System/settings.py
+++ Billing_System/settings.py
@@ -78,13 +78,9 @@ WSGI_APPLICATION = "Billing_System.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'billingsystem_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
